a4a3d605c5bfbfcda069fd97ba89ce632ec0ae04
mmazumder <mmazumder@newroads.org>
2017-02-01 12:31:48 -0500

    testship

--- Player.java
+++ Player.java
@@ -1,3 +1,36 @@
 public class Player{
-  private 
+  //private String color;
+  private boolean win; 
+  private int ships; 
+  
+  public Player(){
+    /*color = "red";
+    suit = "diamonds";
+    rank = 'A';*/
+  }
+  public Player(boolean newWin, int newShips){
+    //color = newColor;
+    win = newWin;
+    ships = newShips;
+  }
+  /*public String getColor(){
+    return color;*/
+  
+  public boolean getWin(){
+    return win;
+  }
+  public void setWin(boolean newWin){
+    win = newWin;
+  }
+  public int getShips(){
+    return ships;
+  }
+  public void setShips(int newShips){
+    ships = newShips;
+  }
+ 
+  public String toString(){
+    return win + " " + ships; 
+  }
+
 }
---
+++ TestPlayer.java
@@ -0,0 +1,8 @@
+public class TestPlayer{
+  public static void main(String[] args){
+    Player player1 = new Player(false, 3);
+    
+    System.out.println(player1);
+    
+  }
+}
